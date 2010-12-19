d77507afbead1b814e8b1a4455af72979ffcc004
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-19 20:16:15 +0200

    AwesomeWM config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -27,3 +27,16 @@ layouts =
     awful.layout.suit.magnifier
 }
 
+tags = 
+{
+    names  = { "main", "www", "im", "coding", "gimp", "office", 7, 8, 9 },
+    layout =
+    {
+        layouts[1], layouts[2], layouts[1], layouts[5], layouts[6],
+        layouts[12], layouts[9], layouts[3], layouts[7]
+    }
+}
+
+for s = 1, screen.count() do
+    tags[s] = awful.tag(tags.names, s, tags.layout)
+
