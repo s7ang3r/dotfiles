4bf2a5298857c0daaec3754fda0274502e4c142c
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-26 09:08:43 +0200

    AwesomeWM config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -62,3 +62,41 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
                                      menu = mymainmenu })
 -- }}}
 
+-- {{{ Wibox
+-- cpu
+cpuwidget = widget({ type = "textbox" })
+cpuwidget.width = 30
+cpuwidget.align = "center"
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1%")
+cpuicon = widget({ type = "imagebox" })
+cpuicon.image = image(beautiful.widget_cpu)
+-- cputhermal
+cputhermalwidget  = widget({ type = "textbox" })
+cputhermalwidget.width = 30
+cputhermalwidget.align = "center"
+vicious.register(cputhermalwidget, vicious.widgets.thermal, " $1°C", 20, { "it87.3328", "core"} )
+-- memory
+memwidget = widget({ type = "textbox" })
+memwidget.width = 30
+memwidget.align = "center"
+-- vicious.register(memwidget, vicious.widgets.mem, "$1% ($2MB/$3MB)", 13)
+vicious.register(memwidget, vicious.widgets.mem, "$1%", 13)
+memicon = widget({ type = "imagebox" })
+memicon.image = image(beautiful.widget_mem)
+-- netwidget
+netwidget = widget({ type = "textbox" })
+netwidget.width = 75
+netwidget.align = "center"
+vicious.register(netwidget, vicious.widgets.net, '<span color="#CC9393">${eth0 down_kb}</span> <span color="#7F9F7F">${eth0 up_kb}</span>', 3)
+dnicon = widget({ type = "imagebox" })
+dnicon.image = image(beautiful.widget_net)
+upicon = widget({ type = "imagebox" })
+upicon.image = image(beautiful.widget_netup)
+-- textclock
+mytextclock = awful.widget.textclock({ align = "center" })
+-- systray
+mysystray = widget({ type = "systray" })
+-- separator
+separator = widget({ type = "textbox" })
+separator.text  = "|"
+
