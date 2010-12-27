13488ea3cb4745d9b6dffd9cb299f6a1c14ae703
Rodion Brodetsky <s7ang3r@gmail.com>
2010-12-27 11:53:07 +0200

    AwesomeWM config

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -57,17 +57,17 @@ theme.titlebar_maximized_button_focus_active  = theme.confdir .. "/titlebar/maxi
 
 -- {{{ Layout
 theme.layout_fairh = "/layouts/fairhw.png"
-theme.layout_fairv = "/home/s7ang3r/.config/awesome/layouts/fairvw.png"
-theme.layout_floating  = "/home/s7ang3r/.config/awesome/layouts/floatingw.png"
-theme.layout_magnifier = "/home/s7ang3r/.config/awesome/layouts/magnifierw.png"
-theme.layout_max = "/home/s7ang3r/.config/awesome/layouts/maxw.png"
-theme.layout_fullscreen = "/home/s7ang3r/.config/awesome/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/home/s7ang3r/.config/awesome/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/home/s7ang3r/.config/awesome/layouts/tileleftw.png"
-theme.layout_tile = "/home/s7ang3r/.config/awesome/layouts/tilew.png"
-theme.layout_tiletop = "/home/s7ang3r/.config/awesome/layouts/tiletopw.png"
-theme.layout_spiral  = "/home/s7ang3r/.config/awesome/layouts/spiralw.png"
-theme.layout_dwindle = "/home/s7ang3r/.config/awesome/layouts/dwindlew.png"
+theme.layout_fairv = "/layouts/fairvw.png"
+theme.layout_floating  = "/layouts/floatingw.png"
+theme.layout_magnifier = "/layouts/magnifierw.png"
+theme.layout_max = "/layouts/maxw.png"
+theme.layout_fullscreen = "/layouts/fullscreenw.png"
+theme.layout_tilebottom = "/layouts/tilebottomw.png"
+theme.layout_tileleft   = "/layouts/tileleftw.png"
+theme.layout_tile = "/layouts/tilew.png"
+theme.layout_tiletop = "/layouts/tiletopw.png"
+theme.layout_spiral  = "/layouts/spiralw.png"
+theme.layout_dwindle = "/layouts/dwindlew.png"
 -- }}}
 
 -- {{{ Widgets
