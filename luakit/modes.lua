f4f071f49ea21a3ad070c8735d8f880a50a609c3
Rodion Brodetsky <s7ang3r@gmail.com>
2011-04-15 08:34:47 +0300

    Luakit config

--- luakit/modes.lua
+++ luakit/modes.lua
@@ -5,3 +5,9 @@ function new_mode(name, mode)
     modes[name] = lousy.util.table.join(modes[name] or {}, mode, { name = name })
 end
 
+function get_mode(name)
+    assert(string.match(name, "^[%w-_]+$"), "invalid mode name: " .. name)
+    return modes[name]
+end
+
+
