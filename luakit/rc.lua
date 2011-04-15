baca19f9bc9faad2807df67fa000cdbb9c93348f
Rodion Brodetsky <s7ang3r@gmail.com>
2011-04-15 08:01:15 +0300

    Luakit config

--- luakit/rc.lua
+++ luakit/rc.lua
@@ -2,3 +2,10 @@ require "lousy"
 
 function warn(...) io.stderr:write(string.format(...) .. "\n") end
 function info(...) if luakit.verbose then io.stderr:write(string.format(...) .. "\n") end end
+
+require "globals"
+
+lousy.theme.init(lousy.util.find_config("theme.lua"))
+theme = assert(lousy.theme.get(), "failed to load theme")
+
+
