1f8a12e171aed224a676b27435d66d018e3fc79d
Rodion Brodetsky <s7ang3r@gmail.com>
2011-04-15 08:40:56 +0300

    Luakit config

--- luakit/modes.lua
+++ luakit/modes.lua
@@ -10,4 +10,36 @@ function get_mode(name)
     return modes[name]
 end
 
+window.init_funcs.modes_setup = function (w)
+    w:add_signal("mode-changed", function (_, name)
+        local leave = (w.mode or {}).leave
+        local mode = modes[name]
+        w.mode = mode
+        if leave then leave(w) end
+        if not mode then
+            error("changed to un-handled mode: " .. name)
+        end
+        w:update_binds(name)
+        if mode.enter then mode.enter(w) end
+        w:emit_signal("mode-entered", mode)
+    end)
+    w.ibar.input:add_signal("changed", function ()
+        local mode = w.mode
+        if mode and mode.changed then
+            mode.changed(w, w.ibar.input.text)
+        end
+    end)
+    w.ibar.input:add_signal("activate", function ()
+        local mode = w.mode
+        if mode and mode.activate then
+            local text, hist = w.ibar.input.text, mode.history
+            if mode.activate(w, text) == false then return end
+            if hist and hist.items and hist.items[hist.len or -1] ~= text then
+                table.insert(hist.items, text)
+            end
+        end
+    end)
+
+end
+
 
