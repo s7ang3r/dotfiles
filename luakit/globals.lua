230860ac3e9773b94ed913d8b29874f08d159115
Rodion Brodetsky <s7ang3r@gmail.com>
2011-04-15 09:42:30 +0300

    Luakit config

---
+++ luakit/globals.lua
@@ -0,0 +1,72 @@
+-- Global variables for luakit
+globals = {
+    homepage            = "http://luakit.org/",
+ -- homepage            = "http://github.com/mason-larobina/luakit",
+    scroll_step         = 40,
+    zoom_step           = 0.1,
+    max_cmd_history     = 100,
+    max_srch_history    = 100,
+ -- proxy must now be set through proxy command; environment variable is broken
+    download_dir        = luakit.get_special_dir("DOWNLOAD") or (os.getenv("HOME") .. "/downloads"),
+    default_window_size = "800x600",
+}
+
+-- Make useragent
+local arch = string.match(({luakit.spawn_sync("uname -sm")})[2], "([^\n]*)")
+local lkv  = string.format("luakit/%s", luakit.version)
+local wkv  = string.format("WebKitGTK+/%d.%d.%d", luakit.webkit_major_version, luakit.webkit_minor_version, luakit.webkit_micro_version)
+local awkv = string.format("AppleWebKit/%s.%s+", luakit.webkit_user_agent_major_version, luakit.webkit_user_agent_minor_version)
+globals.useragent = string.format("Mozilla/5.0 (%s) %s %s %s", arch, awkv, wkv, lkv)
+
+local ca_files = {
+    -- $XDG_DATA_HOME/luakit/ca-certificates.crt
+    luakit.data_dir .. "/ca-certificates.crt",
+    "/etc/certs/ca-certificates.crt",
+    "/etc/ssl/certs/ca-certificates.crt",
+}
+for _, ca_file in ipairs(ca_files) do
+    if os.exists(ca_file) then
+        soup.set_property("ssl-ca-file", ca_file)
+        break
+    end
+end
+
+soup.set_property("ssl-strict", false)
+
+search_engines = {
+    luakit      = "http://luakit.org/search/index/luakit?q={0}",
+    google      = "http://google.com/search?q={0}",
+    duckduckgo  = "http://duckduckgo.com/?q={0}",
+    wikipedia   = "http://en.wikipedia.org/wiki/Special:Search?search={0}",
+    debbugs     = "http://bugs.debian.org/{0}",
+    imdb        = "http://imdb.com/find?s=all&q={0}",
+    sourceforge = "http://sf.net/search/?words={0}",
+}
+
+search_engines.default = search_engines.google
+--search_engines.default = "{0}"
+
+cookie_policy = { always = 0, never = 1, no_third_party = 2 }
+
+domain_props = { --[[
+    ["all"] = {
+        ["enable-scripts"]          = false,
+        ["enable-plugins"]          = false,
+        ["enable-private-browsing"] = false,
+        ["user-stylesheet-uri"]     = "",
+        ["accept-policy"]           = cookie_policy.never,
+    },
+    ["youtube.com"] = {
+        ["enable-scripts"] = true,
+        ["enable-plugins"] = true,
+    },
+    ["lwn.net"] = {
+       ["accept-policy"] = cookie_policy.no_third_party,
+    },
+    ["bbs.archlinux.org"] = {
+        ["user-stylesheet-uri"]     = "file://" .. luakit.data_dir .. "/styles/dark.css",
+        ["enable-private-browsing"] = true,
+    }, ]]
+}
+
+
