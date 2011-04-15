d88991ddc14e3b4132c73cd3e6c319cc2b8ce341
Rodion Brodetsky <s7ang3r@gmail.com>
2011-04-15 08:49:35 +0300

    Luakit config

--- luakit/binds.lua
+++ luakit/binds.lua
@@ -7,4 +7,19 @@ local more, less = "+"..scroll_step.."px", "-"..scroll_step.."px"
 local zoom_step = globals.zoom_step or 0.1
 local homepage = globals.homepage or "http://luakit.org"
 
+function add_binds(mode, binds, before)
+    assert(binds and type(binds) == "table", "invalid binds table type: " .. type(binds))
+    mode = type(mode) ~= "table" and {mode} or mode
+    for _, m in ipairs(mode) do
+        local mdata = get_mode(m)
+        if mdata and before then
+            mdata.binds = join(binds, mdata.binds or {})
+        elseif mdata then
+            mdata.binds = mdata.binds or {}
+            for _, b in ipairs(binds) do table.insert(mdata.binds, b) end
+        else
+            new_mode(m, { binds = binds })
+        end
+    end
+end
 
