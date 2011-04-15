91966989416680f05dc1ae9c86bce9716348bc52
Rodion Brodetsky <s7ang3r@gmail.com>
2011-04-15 08:04:00 +0300

    Luakit config

--- luakit/rc.lua
+++ luakit/rc.lua
@@ -34,3 +34,13 @@ require "go_input"
 require "go_next_prev"
 require "go_up"
 
+local w = (session and session.restore())
+if w then
+    for i, uri in ipairs(uris) do
+        w:new_tab(uri, i == 1)
+    end
+else
+    -- Or open new window
+    window.new(uris)
+end
+
