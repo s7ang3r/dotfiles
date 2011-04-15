72946a953fae5a5e814d935e37b4abff72e63d23
Rodion Brodetsky <s7ang3r@gmail.com>
2011-04-15 08:15:22 +0300

    Luakit config

--- luakit/theme.lua
+++ luakit/theme.lua
@@ -15,4 +15,12 @@ theme.warning_bg = "#FFF"
 theme.notif_fg = "#444"
 theme.notif_bg = "#FFF"
 
+theme.menu_fg                   = "#000"
+theme.menu_bg                   = "#fff"
+theme.menu_selected_fg          = "#000"
+theme.menu_selected_bg          = "#FF0"
+theme.menu_title_bg             = "#fff"
+theme.menu_primary_title_fg     = "#f00"
+theme.menu_secondary_title_fg   = "#666"
+
 
